b7b5a84e731da342556318d3277461a92f3a0e09
Alden Manisan <84431560+ArudenKun@users.noreply.github.com>
2024-09-01 15:16:47 +0800

    Moved files to proper namespaces

--- GodotSharpExtras/Collections/BoxLessQueue.cs
+++ GodotSharpExtras/Collections/BoxLessQueue.cs
@@ -2,7 +2,22 @@
 using System.Collections.Generic;
 using AutoInterfaceAttributes;
 
-namespace GodotSharpExtras.Collections.Queue;
+namespace GodotSharpExtras.Collections;
+
+/// <summary>
+/// Interface for handling values stored in a <see cref="IBoxLessQueue"/>.
+/// </summary>
+public interface IBoxLessValueHandler
+{
+    /// <summary>
+    /// Callback invoked when a value is dequeued from a
+    /// <see cref="BoxLessQueue"/>.
+    /// </summary>
+    /// <param name="value">Value that was dequeued.</param>
+    /// <typeparam name="TValue">Type of the value.</typeparam>
+    void HandleValue<TValue>(in TValue value)
+        where TValue : struct;
+}
 
 /// <summary>
 /// <para>
--- GodotSharpExtras/Collections/Pool.cs
+++ GodotSharpExtras/Collections/Pool.cs
@@ -2,9 +2,22 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using AutoInterfaceAttributes;
+
 #pragma warning disable CS8633 // Nullability in constraints for type parameter doesn't match the constraints for type parameter in implicitly implemented interface method'.
 
-namespace GodotSharpExtras.Collections.Pool;
+namespace GodotSharpExtras.Collections;
+
+/// <summary>
+/// Represents a type that can be stored in a pool. Pools maintain
+/// pre-instantiated instances that can be borrowed and returned to avoid
+/// excess memory allocations. Pooled objects are "reset" whenever they are
+/// returned to the pool so that they can be reused.
+/// </summary>
+public interface IPooled
+{
+    /// <summary>Resets the object to its default state.</summary>
+    void Reset();
+}
 
 /// <summary>
 /// Represents a pool of objects that can be borrowed and returned. Objects
